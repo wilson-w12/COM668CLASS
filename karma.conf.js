f12b41b7574e7a57c61d84b6e0470b3947bb0b4a
William Wilson <wilson-w12@ulster.ac.uk>
2025-04-03 15:13:21 +0100

    Reverting logs changes

--- karma.conf.js
+++ karma.conf.js
@@ -1,29 +1,45 @@
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 module.exports = function(config) {
-  config.set({
+  const configuration = {
     basePath: "",
     frameworks: ["jasmine", "@angular-devkit/build-angular"],
     files: [
-      { pattern: "src/test.ts", watched: false },
-      { pattern: "src/app/**/*.spec.ts", watched: false }
+      "src/test.ts",
+      "src/app/**/*.spec.ts"
     ],
+    exclude: [],
     preprocessors: {
-      "src/app/**/*.ts": ["coverage"]
+      "src/test.ts": ["@angular-devkit/build-angular"]
     },
-    reporters: ["progress", "coverage"], 
-    coverageReporter: {
-      dir: "coverage/", 
-      reporters: [
-        { type: "html", subdir: "html" },
-        { type: "lcovonly", subdir: "lcov" },
-        { type: "text-summary" }
-      ]
+    reporters: ["progress", "junit"],
+    junitReporter: {
+      outputDir: "test-results",
+      outputFile: "karma-test-results.xml",
+      useBrowserName: false
     },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ["ChromeHeadless"],
     singleRun: false,
+    browsers: ["ChromeHeadless", "FirefoxHeadless"],
+    captureTimeout: 60000,
     concurrency: Infinity
-  });
+  };
+
+  if (process.env.INSIDE_DOCKER) configuration.browsers = ["DockerChrome"];
+
+  config.set(configuration);
 };
