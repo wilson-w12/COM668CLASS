337fc60173da85a503109fb91b1a0bb3eb91fc3d
William Wilson <wilson-w12@ulster.ac.uk>
2025-04-10 16:06:27 +0100

    Added popop error handling when fetching records

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../auth/auth.service'; 
+import { PopupNotificationService } from '../../services/popup-notification.service';
 
 @Component({
   selector: 'app-login',
@@ -13,7 +14,7 @@ export class LoginComponent {
   password: string = '';
   loginError: boolean = false;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router, private popupService: PopupNotificationService,) {}
 
   // Submit
   onSubmit(): void {
@@ -27,6 +28,7 @@ export class LoginComponent {
         },
         (error) => {
           this.loginError = true;
+          this.popupService.showError('Unable to login. Please try again.');
         }
       );
     }
